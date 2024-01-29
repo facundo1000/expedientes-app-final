2447260c5ac61d1be9e9d9d9782af81c460f1d6b
André <andregelabert21@gmail.com>
2024-01-29 11:47:55 -0300

    Creacion de la vista listarPersonas

---
+++ src/main/java/edu/unam/expedientesappfinal/controladores/personas/listarPersonasController.java
@@ -0,0 +1,36 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Button btnAgregar;
+
+    @FXML
+    private Button btnMenuConsejo;
+
+    @FXML
+    private Button btnMenuExpedientes;
+
+    @FXML
+    private Button btnMenuMinutas;
+
+    @FXML
+    private Button btnMenuPersonas;
+
+    @FXML
+    private Button btnMenuReuniones;
+
+    @FXML
+    private Pagination btnPaginacion;
+
+    @FXML
+    private TextField inputFiltro;
+
+    @FXML
+    private TableView<?> tablaPersonas;
+
+}
